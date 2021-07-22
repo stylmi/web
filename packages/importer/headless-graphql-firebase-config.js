bb7ab8377154cdd64755670f601802242f6e295a
Ola A <olajire.atose@gmail.com>
2021-07-23 00:04:59 +0200

    fix db issues

---
+++ packages/importer/headless-graphql-firebase-config.js
@@ -0,0 +1,12 @@
+module.exports={
+  "type":"service_account",
+  "project_id":"sellit-201db",
+  "private_key_id":"5ee6f122d4743c5a10f059d8a2662a93219b5ef0",
+  "private_key":"-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDVJBwS8fxkDhy7\nYW9Dye08pF6xKRdQ3xIkDM6DNfUqou+VVxiNvsWVTsuKk0Yr0KsN8TiT983IqtN3\nn1i2cUktTMGpTkNPvUAPAglGBW/MGD1aiJSCIY6ZiGBTLQThl/q1qiY8I3ch7MOj\nLzmY8m3LLIxD26Z/YLzT5lDT0uyIU11iJ0Yls+/mq1fZDdi/oeXh1MlV2TbovLHD\nrHCnxsZzMD5se8t7Dhj6lbk/P7wxQcO1yZPuOCukSm0UTaDkdAsj9lPE+NXp4fsu\ni2s0QrT4ySNpn4+R8Xh5oU0v+ORTZ3yTeKgPOerVPNgOv0sFzU/vAqYcGQhMfAL3\njlJsD/rfAgMBAAECggEAMHvyVuzs+OWs64MSiF0atjy9vZcXUrIrsj+ninlvt68w\nyMvzLetDjt9JtdAimjzxbE5/DEnGaetSB1gcGgRTHRbkxtqivTTTqZZQ/bim4cpx\nynJfNerkQVCgeMs43QhxY3m9ENkQdIsjRa+sbnpXN2x+y8DfcwJScfFmWYkVfTzv\nK92KTyK0J+oTvGhC80dVvJHpLaRRzdsNwR6kMrlhTTEv633uXx1ciN9n76+Xoy3C\nhwVRyuIUubbPQrIaLpMJ4j7tttOuhL1dA3EXmPTZueB6Qlkd3M1i9cDt/iKKYs/I\n+Kst1dXyD8t04uD62DOenxtOmdsy2gJzaFexH8ktQQKBgQDtVKgZmgPGCjAv712Z\n+9dWipOCG1f33xxnMd1dnno/Z4+Zre75cdztznphQkAw5CqycxU5ug45nHNF8eSn\nGoK7OxRPGCrk8V4ESeyuEWJrliDQApl58IUJ1JXeP2CznkrHFTEFbKYPePIVi6JG\noNhhlYoykUOuXwuAp5hMlDRWZwKBgQDl6FMJ++goB8jqq1HhA36S2HaM0S42RuGN\nMaOSf2pE2tD7EsxkvEQh7gOQU/cbbfe3Zz/GYkFxhNxJqJpY6Pf3jhA+9gB49zZi\nuCSPN65N8M3CJcfrFGuLGKX8UNExO9czUmK5WnV/odjLeF2xDjrxvLEFWMw0IN1J\nUnMCfIg8yQKBgQC0NE2XcuxemSnRm+z0mNb/LSWn6t82pMBKUwCEr2qfbiEJTZpg\n6WYZY2hlJQrclMMe0GdTd8SJVhPpDizR/T06sTdQf0dCo0sB8qe13BlhG5M6nl6u\nykQaHMLXCESC3jRlSd/5AHBu2yTl2yqSelsSbZuLneVIJ37yZ08rBnVfDQKBgQDS\nGsLoeOHs01B1xSMKs5rUsCWHAxNsXtJ5qkeF9hMz4ZTiHofT94l8SHvRRiqpUpHz\nddP+hISyLWzzk7WaUy1peta18zLzWVjGVupyR7xvVgVzy/srhnnRoDDwqRrEF5/P\nMxcttNF0VoA9Uj6tNAgCKyLLdB8J9I7GFycruor2oQKBgBshoRXIQvn8x/UDM0op\npxV5zJM5asepH9LVkCNPabVOGRHbpBDSbO2lcCGtiZZAYIMdPJ9CZrkl3QnXDfYN\nEA9pHaNwC07mBDThVpDOwN5hhPgTx/GWjDLrL4PswoZxBBYQxLbkt8xgHO8Fiqih\n8KNprBtyA7EdsYjpMBK+e0JB\n-----END PRIVATE KEY-----\n",
+  "client_email":"firebase-adminsdk-cvycm@sellit-201db.iam.gserviceaccount.com",
+  "client_id":"103981733065031223625",
+  "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+  "token_uri":"https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url":"https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cvycm%40sellit-201db.iam.gserviceaccount.com"
+}
